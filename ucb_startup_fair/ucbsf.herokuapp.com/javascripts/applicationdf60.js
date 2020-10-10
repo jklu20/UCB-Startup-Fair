97b0aca380aa3b1ffec2309292cb5c52bb59d5b5
jklu20 <71949989+jklu20@users.noreply.github.com>
2020-10-09 17:03:30 -0700

    Added webscrape of the site

---
+++ ucb_startup_fair/ucbsf.herokuapp.com/javascripts/applicationdf60.js
@@ -0,0 +1,13 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function() {
+//  jQuery('#messages').delay(500).animate({
+//      opacity: 'toggle',
+//      height: 'toggle'
+//    }, 'slow', function() {
+//  jQuery(this).delay(2500).animate({
+//      opacity: 'toggle',
+//      height: 'toggle'
+//    }, 'slow');
+//   });
+});
